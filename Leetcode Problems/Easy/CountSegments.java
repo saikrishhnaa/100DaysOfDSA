b992d0d644d495f78cf2987a0dd71a432e113734
saikrishhnaa <saikrishnaamakam@gmail.com>
2022-10-11 08:56:30 -0700

    Completed with count segments program

---
+++ Leetcode Problems/Easy/CountSegments.java
@@ -0,0 +1,36 @@
+/*
+Description:
+
+Given a string s, return the number of segments in the string.
+
+A segment is defined to be a contiguous sequence of non-space characters.
+
+Example 1:
+
+Input: s = "Hello, my name is John"
+Output: 5
+Explanation: The five segments are ["Hello,", "my", "name", "is", "John"]
+Example 2:
+
+Input: s = "Hello"
+Output: 1
+ 
+Constraints:
+
+0 <= s.length <= 300
+s consists of lowercase and uppercase English letters, digits, or one of the following characters "!@#$%^&*()_+-=',.:".
+The only space character in s is ' '.
+*/
+
+class Solution {
+    public int countSegments(String s) {
+        int segmentCount = 0;
+        s = " " + s;
+        for(int i = 0; i < s.length() - 1; i++)
+        {
+            if(s.charAt(i) == ' ' && s.charAt(i + 1) != ' ')
+                segmentCount++;
+        }
+        return segmentCount;
+    }
+}

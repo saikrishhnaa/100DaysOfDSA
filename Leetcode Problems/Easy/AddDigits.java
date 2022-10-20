ac6079e851bb7b7fc1d271b1e04c580b46dc939b
saikrishhnaa <saikrishnaamakam@gmail.com>
2022-10-20 11:31:11 -0700

    Submit Add digits program code

---
+++ Leetcode Problems/Easy/AddDigits.java
@@ -0,0 +1,41 @@
+/*
+Description:
+
+Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
+
+Example 1:
+
+Input: num = 38
+Output: 2
+Explanation: The process is
+38 --> 3 + 8 --> 11
+11 --> 1 + 1 --> 2 
+Since 2 has only one digit, return it.
+Example 2:
+
+Input: num = 0
+Output: 0
+ 
+
+Constraints:
+
+0 <= num <= 231 - 1
+*/
+
+class Solution 
+{
+    public int addDigits(int num) 
+    {
+        while(num >= 10)
+        {
+            int val = num;
+            num = 0;
+            while(val != 0)
+            {
+                num += val % 10; 
+                val /= 10;
+            }
+        }
+        return num;
+    }
+}

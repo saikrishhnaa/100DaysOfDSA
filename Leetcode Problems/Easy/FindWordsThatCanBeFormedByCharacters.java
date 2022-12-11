6d111d0ce43f379d97f897b17204b91ae9fba89a
saikrishhnaa <saikrishnaamakam@gmail.com>
2022-12-11 09:48:16 -0800

    In progress - Find words that can be formed by characters program

---
+++ Leetcode Problems/Easy/FindWordsThatCanBeFormedByCharacters.java
@@ -0,0 +1,64 @@
+/*
+Description:
+
+You are given an array of strings words and a string chars.
+
+A string is good if it can be formed by characters from chars (each character can only be used once).
+
+Return the sum of lengths of all good strings in words.
+
+Example 1:
+
+Input: words = ["cat","bt","hat","tree"], chars = "atach"
+Output: 6
+Explanation: The strings that can be formed are "cat" and "hat" so the answer is 3 + 3 = 6.
+Example 2:
+
+Input: words = ["hello","world","leetcode"], chars = "welldonehoneyr"
+Output: 10
+Explanation: The strings that can be formed are "hello" and "world" so the answer is 5 + 5 = 10.
+ 
+Constraints:
+
+1 <= words.length <= 1000
+1 <= words[i].length, chars.length <= 100
+words[i] and chars consist of lowercase English letters.
+*/
+
+import java.util.List;
+import java.util.ArrayList;
+
+class Solution 
+{
+    public int countCharacters(String[] words, String chars) 
+    {
+        int charCount = 0;
+        List<Character> charsList = new ArrayList<Character>();
+        for(int i = 0; i < chars.length(); i++)
+        {
+            charsList.add(chars.charAt(i));
+        }
+        for(String word : words)
+        {
+            for(int i = 0; i < word.length(); i++)
+            {
+                boolean isCharPresent = false;
+                for(int j = 0; j < charsList.size(); j++)
+                {
+                    if(word.charAt(i) == charsList.get(j))
+                    {
+                        isCharPresent = true;
+                        charsList.remove((Character)word.charAt(i));
+                        j--;
+                        break;
+                    }
+                }
+                if(!isCharPresent)
+                    break;
+                if(i == word.length())
+                    charCount += word.length();
+            }
+        }
+        return charCount;
+    }
+}
